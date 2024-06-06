3cd7d5057630cfc77e0b1cceef5258ead42b99c3
rt65s <rt65s@SCHOOL_LAPTOP>
2024-06-05 18:04:46 -0700

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Prog122_L15_Final_Review
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
